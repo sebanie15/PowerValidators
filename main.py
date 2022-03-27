e7a0f7f211a1581f8820a3c2e6bd28d479217c2b
Sebastian Nieciąg <sebanie15@gmail.com>
2022-03-27 12:16:52 +0200

    transfer of print_progress_bar function to utils.py module

--- main.py
+++ main.py
@@ -4,32 +4,7 @@
 from wsgiref.validate import validator
 from PwdValidator import PwdValidator, PwdFileManager, haveibeenpwned, hash_pwd
 
-
-# Print iterations progress - source: stackoverflow
-def print_progress_bar(
-    iteration: int,
-    total: int,
-    prefix: str = "Progress",
-    suffix: str = "Complete",
-    length: int = 50
-):
-    """
-    Call in a loop to create terminal progress bar
-    Args:
-        iteration (int): current iteration
-        total (int) : total iterations
-        prefix (str) : prefix string
-        suffix (str) : suffix string
-        length (str) : character length of bar
-    """
-    fill: str = "█"
-    percent = ("{0:." + str(1) + "f}").format(100 * (iteration / total))
-    filled_length = int(length * iteration // total)
-    prog_bar = fill * filled_length + "-" * (length - filled_length)
-    print(f"\r{prefix} |{prog_bar}| {percent}% {suffix}", end='\r')
-    # Print New Line on Complete
-    if iteration == total:
-        print()
+from .utils import print_progress_bar
 
 
 validator = PwdValidator()
